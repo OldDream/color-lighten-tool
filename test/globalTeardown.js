cffc0f0c619828124408d230746cafa22ceb03fe
Old_dream <huangyn15@qq.com>
2022-04-18 23:05:36 +0800

    add: test

--- test/globalTeardown.js
+++
@@ -1,6 +0,0 @@
-// globalTeardown.js  用于停止本地服务
-const { teardown: teardownDevServer } = require('jest-dev-server')
-
-module.exports = async function globalTeardown() {
-  await teardownDevServer()
-}
--- test/test.js
+++ test/test.js
@@ -1,32 +1,13 @@
 // test.js  jest配置文件
+import colorLightenTool from  '../src/index'
 
-const puppeteer = require('puppeteer');
-const startServer = require('./globalSetup');
-const stopServer = require('./globalTeardown');
-
-let browser; // 浏览器实例
-let page; // 页面实例
-beforeAll(async () => {
-  await startServer(); // 启动本地服务
-  browser = await puppeteer.launch();
-  page = await browser.newPage();
-  await page.goto('http://127.0.0.1:3000');
+test('Input rgb rgb(255,0,0) + 0.4 opacity', () => {
+  colorLightenTool()
+  expect(colorLightenTool('rgb(255,0,0)', 0.4)).toBe('rgb(255,153,153)')
 });
 
-describe('Test in browser', () => {
-  test('Click copy btn.', async () => {
-    let alertMsg = ''; // 存储弹窗信息
-    // 监听弹窗，可以是是alert、confirm、prompt任意一种
-    await page.on('dialog', async (dialog) => {
-      alertMsg = dialog.message(); // 获取弹框内的信息
-      await dialog.dismiss(); // 关闭弹窗
-    });
-    await page.click('#test'); // 点击按钮
-    expect(alertMsg).toBe('Success! Try to paste.'); // 设置测试预期结果。
-  }, 6000);
-});
 
-afterAll(async () => {
-  await browser.close(); // 关闭浏览器
-  await stopServer(); // 停止本地服务器
+test('Input HEX #FF0000 + 0.4 opacity', () => {
+  colorLightenTool()
+  expect(colorLightenTool('#FF0000', 0.4)).toBe('rgb(255,153,153)')
 });
